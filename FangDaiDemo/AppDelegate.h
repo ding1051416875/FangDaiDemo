73221bdd909945e5af0acbfbe9eec7abdaf577bc
baiteng-5 <baiteng-5@192.168.1.130>
2014-01-06 14:01:48 +0800

    Initial Commit

---
+++ FangDaiDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FangDaiDemo
+//
+//  Created by baiteng-5 on 14-1-6.
+//  Copyright (c) 2014年 org.baiteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
